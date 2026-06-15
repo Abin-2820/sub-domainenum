728bcf015f073eac3658f957d05e3e61a560c782
abin28 <infoabin28@gmail.com>
2026-06-15 16:20:27 +0530

    Update subdomain-Enumerator.py

--- subdomain-Enumerator.py
+++ subdomain-Enumerator.py
@@ -1,56 +1,52 @@
 import sys
 import requests
 import threading
-from concurrent.futures import ThreadPoolExecutor
+
 # ---Argument---
-if len(sys.argv) !=3:
-    print(f"usage: {sys.argv[0]} <domain> <wordlist>")
+if len(sys.argv)!=3:
+    print(f"usage: python3 {sys.argv[0]} <domain> <wordlist>")
     exit()
 domain = sys.argv[1]
 wordlist = sys.argv[2]
-# print(domain)
 
-# ---Loading Wordlist---
+# ---Wordlist---
 try:
-    with open(wordlist,"r") as file:
-        subdomains = [line.strip() for line in file if line.strip()]
-except:
-    print(f"[!] Wordlist not found: {wordlist}")
+    subdomains = []
+    with open(wordlist, "r") as file:
+        for line in file:
+            if line.strip():
+                subdomains.append(line.strip())
+        # for i in subdomains :
+        #     print(i)
+except FileNotFoundError:
+    print(f"[!] wordlist not found: {wordlist}")
     exit()
 
-    total = len(subdomains)
-    checked = 0
-    checked_lock = threading.Lock()
+# ---Core function---
+total = len(subdomains)
+lock = threading.Lock()
 
-    # ---Prograss Listener---
-    def progress_watcher():
-        while True:
-            input()
-            with checked_lock:
-                print(f"\n[*] Prograss: {checked}/{total} checked")
-    
-    watcher = threading.Thread(target=progress_watcher, daemon=True)
-    watcher.start()
+def check(sub):
+    fullDomain = f"{sub}.{domain}"
+    url = f"https://{fullDomain}"
+    try:    
+        requests.get(url, timeout=3)
+        print(f"[+] found: {fullDomain}")
+    except:
+        pass
+threads = []
+print(f"[*] scanning {domain} | {total} subdomains\n")
 
-    # ---Core Function---
-    def check_subdomain(sub):
-        global checked
-        full_domain = f"{sub}.{domain}"
-        url = f"https://{full_domain}"
-        try:
-            response = requests.get (url, timeout=3)
-            print(f"[+] found: {full_domain} (Status: {response.status_code})")
-        except requests.ConnectionError:
-            pass
-        except requests.Timeout:
-            pass
-        finally:
-            with checked_lock:
-                checked += 1
-    
-    # ---Run with threading---
-    print(f"[*] starting enumeration on {domain} | {total} subdomains | press Enter for progress\n")
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        executor.map(check_subdomain, subdomains)
+for sub in subdomains:
+    t = threading.Thread(target=check, args=(sub, ))
+    threads.append(t)
+    t.start()
 
-    print(f"\n[*] Done. {total} subdomains checked.") 
+    for t in threads:
+        t.join()
+
+print(f"\n[*] Done. {total} subdomains checked.")
+
+
+
+
